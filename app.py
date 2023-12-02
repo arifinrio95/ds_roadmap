fb203b6f2913fe5623f8a1c4eb1a818ccc29bfda
Rio Nur Arifin <59592379+arifinrio95@users.noreply.github.com>
2023-12-02 22:20:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 import streamlit as st
-from _01_web_scraping_content import content as wsc
+from _01_pandas import content as cont_pandas
 
 def main():
     st.title("Data Science Roadmap")
 
     # Data structure for topics and sub-topics
     topics = {
-        "Data Collection": {
-            "Web Scraping": wsc,
-            "APIs": "Learn about REST APIs, authentication"
+        "Python Programming": {
+            "Pandas": cont_pandas,
+            "Numpy": "Learn about Numpy"
         },
-        "Data Cleaning": {
-            "Pandas Basics": "Learn about DataFrame operations",
-            "Data Transformation": "Learn about data normalization, transformation"
+        "Machine Learning": {
+            "Klasifikasi": "Placeholder content.",
+            "Regresi": "Placeholder content.",
+            "Clustering": "Placeholder content.",
+            "Deep Learning": "Placeholder content.",
+        },
+        "Statistika": {
+            "Statistika untuk Data Science": "Placeholder content."
+        },
+        "Data Visualisasi": {
+            "Data Visualisasi dengan Matplotlib": "Placeholder content.",
+            "Data Visualisasi dengan Seaborn": "Placeholder content."
+        },
+        "SQL": {
+            "SQL": "Placeholder content."
+        },
+        "Natural Language Programming": {
+            "NLP": "Placeholder content."
         },
         # More topics and sub-topics can be added here
     }
